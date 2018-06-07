39b0de18641184f4293e8be97d34aed5116d56c9
Grace Shih <vocalise@gmail.com>
2018-06-07 03:36:02 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -58,8 +58,9 @@ class Dog
       WHERE id = ?
      SQL
     
-    row = DB[:conn].execute(sql, id)
-    
+    row = DB[:conn].execute(sql, id)[0]
+    d = Dog.new(row[0], row[1], row[2])
+    d
   end
   
 '''  
