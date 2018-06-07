7d234b8ca5888c40099255ce26238447a4f49acb
Grace Shih <vocalise@gmail.com>
2018-06-07 03:41:42 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -52,7 +52,7 @@ class Dog
      SQL
     
     row = DB[:conn].execute(sql, id)
-    self.create(id: row[0], name: row[1], breed: row[2])
+    Dog.new(id: row[0], name: row[1], breed: row[2])
   end
   
 '''
