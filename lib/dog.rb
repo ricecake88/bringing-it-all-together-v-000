bc35d783ee7276dfcf2bb35920e4dc3b4109c109
Grace Shih <vocalise@gmail.com>
2018-06-07 04:10:15 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -80,11 +80,12 @@ class Dog
   end
 
 
-  def self.create_table
+  def self.update
     sql=<<-SQL
+      UPDATE dogs SET name = ?, breed = ? WHERE id = ?
      SQL
     
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql, self.name, self.)
   end  
   
 '''  
