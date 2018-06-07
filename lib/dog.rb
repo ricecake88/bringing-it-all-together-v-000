8611fefed58c6dd3ec93c5d67ef1837575d05efc
Grace Shih <vocalise@gmail.com>
2018-06-07 04:08:17 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -74,7 +74,9 @@ class Dog
     SELECT * FROM dogs WHERE name = ?
      SQL
     
-    DB[:conn].execute(sql,name)
+    DB[:conn].execute(sql,name) do |row|
+      new_from_db(row)
+    end
   end
 
 '''  
