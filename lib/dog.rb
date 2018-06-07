d1f210da9179352cc8defba03c71387a6e8af430
Grace Shih <vocalise@gmail.com>
2018-06-07 03:47:04 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -53,7 +53,7 @@ class Dog
     
     row = DB[:conn].execute(sql, id)[0]
     #d = self.create(name: row[1], breed: row[2])
-    d = Dog.new(name: row[1], breed: row[2])
+    d = Dog.new(id: row[0], name: row[1], breed: row[2])
     binding.pry
   end
   
